d43c31595b14c24e155263e01de87874a6cbae97
destroyergm <stefan_vg@hotmail.com>
2016-06-17 13:21:51 +0200

    Add edit user page, user tests

--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -1,7 +1,22 @@
 require 'test_helper'
 
 class UserTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+ 	def setup
+ 		@user = User.new(email: 'test@example.com', password: 'password', 
+ 			password_confirmation: 'password', first_name: 'stefan', last_name: 'stefan')
+ 	end
+
+ 	test 'should be valid' do
+ 		assert @user.valid?
+ 	end
+
+ 	test 'first name should be present' do
+ 		@user.first_name = "    "
+ 		assert_not @user.valid?
+ 	end
+
+ 	test 'last name should be present' do
+ 		@user.last_name = "    "
+ 		assert_not @user.valid?
+ 	end	
 end
