ae78112506da42b48e800eb409787c4c2fb27582
destroyergm <stefan_vg@hotmail.com>
2016-06-18 17:12:55 +0200

    Tweek friendship record

--- db/migrate/20160618134803_create_friendships.rb
+++ db/migrate/20160618134803_create_friendships.rb
@@ -1,14 +1,14 @@
 class CreateFriendships < ActiveRecord::Migration
   def change
     create_table :friendships do |t|
-    	t.integer :from_user_id, null: false
-    	t.integer :to_user_id, null: false
+    	t.integer :user_id, null: false
+    	t.integer :friend_id, null: false
     	t.string :status, null: false
 
       t.timestamps null: false
     end
 
-    add_index :friendships, :from_user_id
-    add_index :friendships, :to_user_id
+    add_index :friendships, :user_id
+    add_index :friendships, :friend_id
   end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -17,15 +17,15 @@ ActiveRecord::Schema.define(version: 20160618134803) do
   enable_extension "plpgsql"
 
   create_table "friendships", force: :cascade do |t|
-    t.integer  "from_user_id",                 null: false
-    t.integer  "to_user_id",                   null: false
-    t.boolean  "accepted",     default: false
-    t.datetime "created_at",                   null: false
-    t.datetime "updated_at",                   null: false
+    t.integer  "user_id",    null: false
+    t.integer  "friend_id",  null: false
+    t.string   "status",     null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
-  add_index "friendships", ["from_user_id"], name: "index_friendships_on_from_user_id", using: :btree
-  add_index "friendships", ["to_user_id"], name: "index_friendships_on_to_user_id", using: :btree
+  add_index "friendships", ["friend_id"], name: "index_friendships_on_friend_id", using: :btree
+  add_index "friendships", ["user_id"], name: "index_friendships_on_user_id", using: :btree
 
   create_table "users", force: :cascade do |t|
     t.string   "email",                  default: "", null: false
