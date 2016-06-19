761fe198deb02b4516f64354d180667ecfc527a9
destroyergm <stefan_vg@hotmail.com>
2016-06-19 15:48:36 +0200

    Add Relationship tests

--- test/models/relationship_test.rb
+++ test/models/relationship_test.rb
@@ -1,7 +1,21 @@
 require 'test_helper'
 
 class RelationshipTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  def setup
+  	@relationship = Relationship.create(following_id: 1, follower_id: 2)
+  end
+
+  test 'should be valid' do
+  	assert @relationship.valid?
+  end
+
+  test 'should require following id' do
+  	@relationship.following_id = nil
+  	assert_not @relationship.valid?
+  end
+
+  test 'should require follower id' do
+  	@relationship.follower_id = nil
+  	assert_not @relationship.valid?
+  end
 end
